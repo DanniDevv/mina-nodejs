cfa72ee2a56c27f9542b2609344e07707901d568
DaniDev <dante.rodriguez.c@tecsup.edu.pe>
2024-05-17 21:53:39 -0500

    facilidad de rutas

--- src/app.js
+++ src/app.js
@@ -8,6 +8,19 @@ app.use(express.json())
 
 app.use('/api', require('./routes'))
 
+app.get('/', (req, res) => {
+  res.send(
+    `<h1>API</h1>
+    <ul>
+      <li><a href="/api/usuarios">/api/usuarios</a></li>
+      <li><a href="/api/resultados">/api/resultados</a></li>
+      <li><a href="/api/pruebas">/api/pruebas</a></li>
+      <li><a href="/api/confirmacion">/api/confirmacion</a></li>
+      <li><a href="/api/epps">/api/epps</a></li>
+      <li><a href="/api/epplista">/api/epplista</a></li>
+    </ul>
+  `)
+})
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
